bf969da5bfb47004b43a29a4c39dc0f35f0e5d61
DanielmLarsson <110419426+DanielmLarsson@users.noreply.github.com>
2023-09-12 14:14:20 +0200

    LessonA3

---
+++ LessonA3.ps1
@@ -0,0 +1,33 @@
+﻿
+function Get-ExpiringCertificates
+{
+Param([parameter(Mandatory=$true)] $months)
+
+
+#Calculate Months to days
+#Current Month
+$DaysInMonthOne = [datetime]::DaysInMonth((Get-Date).Year,(Get-Date).Month)
+#NextMonth
+$DaysInMonthTwo = [datetime]::DaysInMonth((Get-Date).Year,(Get-Date).AddMonths(1).Month)
+#LastMonth
+$DaysInMonthThree = [datetime]::DaysInMonth((Get-Date).Year,(Get-Date).AddMonths(2).Month)
+#CombineDays
+$DaysTotalInThreeMonths = $DaysInMonthOne+$DaysInMonthTwo+$DaysInMonthThree
+
+Write-Host "DaysTotalInThreeMonths is " $DaysTotalInThreeMonths
+
+#Check certificates that will expire in next X months
+$AllLocalCertificates = Get-ChildItem Cert:\LocalMachine\ -Recurse -ExpiringInDays $DaysTotalInThreeMonths
+
+
+Write-Host "Certificates that will expire in " $months "months, which is" $DaysTotalInThreeMonths "days"
+foreach($Certificate in $AllLocalCertificates)
+{
+#Write out the certificates
+Write-Host $Certificate | Select-Object FriendlyName, Subject
+
+}
+
+}
+
+Get-ExpiringCertificates -months 3

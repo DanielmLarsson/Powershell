080da7791944eddeb84d403646a4e94e6e328d08
DanielmLarsson <110419426+DanielmLarsson@users.noreply.github.com>
2023-09-14 13:33:08 +0200

    LessonA3UpdateWithA3.1
    Did some wrong things in LessonA3, now probably fixed.

--- LessonA3.ps1
+++ LessonA3.ps1
@@ -4,23 +4,45 @@ function Get-ExpiringCertificates
 Param([parameter(Mandatory=$true)] $months)
 
 
-#Calculate Months to days
-#Current Month
-$DaysInMonthOne = [datetime]::DaysInMonth((Get-Date).Year,(Get-Date).Month)
-#NextMonth
-$DaysInMonthTwo = [datetime]::DaysInMonth((Get-Date).Year,(Get-Date).AddMonths(1).Month)
-#LastMonth
-$DaysInMonthThree = [datetime]::DaysInMonth((Get-Date).Year,(Get-Date).AddMonths(2).Month)
-#CombineDays
-$DaysTotalInThreeMonths = $DaysInMonthOne+$DaysInMonthTwo+$DaysInMonthThree
 
-Write-Host "DaysTotalInThreeMonths is " $DaysTotalInThreeMonths
+#Calculate Months to days
+$MonthsChecked = 0;
+$DaysInMonthAddedTogether = 0
+
+while($MonthsChecked -le $months)
+{
+
+
+#if check is zero
+if($MonthsChecked -eq 0){
+    #Current Month
+    $DaysInMonth = [datetime]::DaysInMonth((Get-Date).Year,(Get-Date).Month)
+    #Add to check
+    $MonthsChecked++
+}
+
+#If check is less or equal to
+if($MonthsChecked -le $months)
+{
+$NextDaysInMonth = [datetime]::DaysInMonth((Get-Date).Year,(Get-Date).AddMonths($MonthsChecked).Month)
+#Add days to total count
+$DaysInMonthAddedTogether +=$NextDaysInMonth
+#Add to check
+$MonthsChecked++
+}
+}
+
+
+#Write-Host "DaysTotalCombined is " $DaysInMonthAddedTogether
 
 #Check certificates that will expire in next X months
-$AllLocalCertificates = Get-ChildItem Cert:\LocalMachine\ -Recurse -ExpiringInDays $DaysTotalInThreeMonths
+$AllLocalCertificates = Get-ChildItem Cert:\LocalMachine\ -Recurse -ExpiringInDays $DaysInMonthAddedTogether | Format-Table -AutoSize | Out-String
 
 
-Write-Host "Certificates that will expire in " $months "months, which is" $DaysTotalInThreeMonths "days"
+
+Write-Host "Certificates that will expire in " $months "months, which is" $DaysInMonthAddedTogether "days"
+
+
 foreach($Certificate in $AllLocalCertificates)
 {
 #Write out the certificates
@@ -30,4 +52,5 @@ Write-Host $Certificate | Select-Object FriendlyName, Subject
 
 }
 
-Get-ExpiringCertificates -months 3
+#Get-ExpiringCertificates -months 200
+Get-ExpiringCertificates -months 200 *> c:\temp\TaskLog.txt

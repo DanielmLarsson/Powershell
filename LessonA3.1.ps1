98738e97d3bd7607531c82dbb8415af426033618
DanielmLarsson <110419426+DanielmLarsson@users.noreply.github.com>
2023-09-14 13:34:05 +0200

    LessonA3.1

---
+++ LessonA3.1.ps1
@@ -0,0 +1,16 @@
+﻿function Start-ScheduledTaskCert
+{
+Param([parameter(Mandatory=$false)] $time)
+
+#Task variables
+$certTrigger = New-ScheduledTaskTrigger -Daily -At 0am
+$certSettings = New-ScheduledTaskSettingsSet
+$certAction = New-ScheduledTaskAction -Execute 'powershell.exe' -WorkingDirectory "C:\temp" -Argument '-NonInteractive -NoLogo -NoProfile -Command " .\LessonA3ForTask.ps1' 
+$certTask = New-ScheduledTask -Action $certAction -Trigger $certTrigger -Settings $certSettings
+
+#create the task
+Register-ScheduledTask -TaskName 'Cert Check Daily Run' -InputObject $certTask
+
+}
+
+Start-ScheduledTaskCert 

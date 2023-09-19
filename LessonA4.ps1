68dfd831d1348c066f30eccfe03929c1e4191873
DanielmLarsson <110419426+DanielmLarsson@users.noreply.github.com>
2023-09-19 19:48:27 +0200

    LessonA4

---
+++ LessonA4.ps1
@@ -0,0 +1,28 @@
+﻿
+
+function Get-FileSizes
+{
+Param([parameter(Mandatory=$true)] $folder,
+[switch] $recurse)
+
+$AllFoldersAndFiles = Get-ChildItem -Path $folder -Recurse | Sort-Object -Property Length -Descending
+
+foreach($fileorfolder in $AllFoldersAndFiles)
+{
+#if a folder
+if($fileorfolder.Attributes -contains "Directory")
+{
+$Length = (Get-ChildItem $FileOrFolder.FullName -Recurse | Where-Object {$_.Attributes -notcontains "Directory"} | Measure-Object -Sum -Property Length).Sum
+$fileorfolder | Select-Object Name, @{Name="Length"; Expression={$Length}}
+
+}else{
+#Its a file
+$fileorfolder | Select-Object Name, Length
+}
+
+}
+
+
+
+}
+Get-FileSizes -folder "C:\temp" -recurse

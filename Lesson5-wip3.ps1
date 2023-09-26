21fca1e8cd0d40590e0199a254727df7327d894a
DanielmLarsson <110419426+DanielmLarsson@users.noreply.github.com>
2023-09-26 16:11:24 +0200

    Lesson5-wip3
    Done up to step 4  in the Lesson 5 assignment

---
+++ Lesson5-wip3.ps1
@@ -0,0 +1,86 @@
+﻿<#
+[relection.assembly]::loadwithpartialname("microsoft.visualbasic") | Out-Null
+
+#Asking for input in our box
+$ValueFromInputBox = [Microsoft.VisualBasic.Interaction]::InputBox("Enter Username to search for here","FindNestedADGroups tool", 0)
+$UserWeLookedAfter = Get-FindADUser
+
+#Find ADUser
+function Get-FindADUser()
+{  
+#Get-ADUser -Filter {name -like '$ValueFromInputBox'} | Format-Table SamAccountName 
+
+    try {
+      Get-ADUser -Identity $ValueFromInputBox
+      Write-Host User exists
+      $UserExists = $true
+      Write-Host We run NestedADGroups
+      Get-NestedADGroups $ValueFromInputBox
+
+    }
+    catch [Microsoft.ActiveDirectory.Management.ADIdentityResolutionException] {
+      Write-Host "User does not exist."
+      $UserExists = $false
+    }
+}
+#>
+
+#Step 4 done in the assignment. Next is 5!(Haven't started that)
+
+#variable
+$results = [System.Collections.ArrayList]::new()
+    
+function Get-NestedADGroups()
+{
+Param([parameter(Mandatory=$true)] $searchString,  
+    [parameter(Mandatory=$false)] $user)
+
+#Find group from name
+$findGroup = Get-ADGroup -Identity $searchString -Properties CN | Select-Object -ExpandProperty CN
+#Write-Host $findGroup
+
+#Find groups that one is a member of
+$groupsInFindGroup = Get-ADGroupMember -Identity $findGroup |?{$_.objectClass -eq "Group"}| Get-ADGroup | Select-Object -ExpandProperty Name
+#Write-Host $groupsInFindGroup 
+
+    #We check through each object in $groupsInFindGroup
+    Foreach($group in $groupsInFindGroup)
+    {
+        #Write-Host "Recursive check Group is:" $group
+       
+        #If the $group already is in results
+        if($results -contains $group)
+        {
+             
+        }else
+        {
+        #we add group to results since it wasn't there
+        [void]$results.Add($group)
+        
+        #Write-Host "Results in loop is" $results     
+
+        #We call the Get-NestedADGroups recursive to go through each group level
+        $nestedGroups = Get-NestedADGroups $group
+        }
+
+        
+    }
+
+
+#Write out the results
+Write-Output $results | Format-Table | Sort-Object -Property Name -Descending
+#Write-Host "Results out of loop is" $results 
+
+
+}
+
+Get-NestedADGroups TestGroup
+
+
+#Get-NestedADGroups testad
+#Get-FindADUser
+
+
+
+
+
